49f9dc5868f8c896ca4123d752497b4ae35a16a6
webwarrior <reg@webwarrior.ws>
2023-05-16 14:09:11 +0200

    Make minimal example

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,12 @@
 
 public partial class MainPage : ContentPage
 {
-	public static readonly BindableProperty RacesProperty = BindableProperty.Create("Races", typeof(string[]), typeof(MainPage), new string[] { "option1", "option2" });
-
-    public string[] Races
-    {
-        get => (string[])GetValue(RacesProperty);
-        set => SetValue(RacesProperty, value);
-    }
-
     public MainPage()
 	{
 		InitializeComponent();
-        this.racePicker.BindingContext = this;
-        this.racePicker.SetBinding(RacesProperty, new Binding("Races"));
-		this.saveButton.Clicked += (obj, args) => 
+		this.openDialogButton.Clicked += (obj, args) => 
 		{
 			this.DisplayAlert("Alert", "message", "cancel");
-
         };
 	}
 }
--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace TemplateMauiApp;
 
-[Activity(Theme = "@style/ImprovedTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
